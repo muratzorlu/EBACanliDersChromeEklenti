1e0a8b5a829ac6b39b65f9a8ec3f7b04f6ca941b
muratzorlu <murtipan@gmail.com>
2020-09-04 12:04:33 +0300

    Update content-script.js

--- js/content-script.js
+++ js/content-script.js
@@ -1,31 +1,53 @@
-var $r= $('<input type="button" value="MAC&PARDUS Zoom Aç" style="position: fixed;top: 55px;right:10px;" id="ekle"/>').click(goFrame);
 
 
-/*var $sayfa=0;
-	var arr = window.location.toString().split('/');
+var $btn= $('<input type="button" value="MAC&PARDUS Zoom Aç" style="position: fixed;top: 55px;right:10px;" id="ekle"/>').click(goFrame);
+
+
+var $url="https://uygulama-ebaders.eba.gov.tr/ders/FrontEndService//studytime/getteacherstudytime";
+var $buton=0;
+
+
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+   if (msg.action == 'SendIt') {
+	  
+	  var arr = window.location.toString().split('/');
 $.each( arr, function( index, value ) {
 	console.log(value);
-    if(value.substring(0,15)=="livesessionview") $sayfa=1;
+    if(value.substring(0,15)=="livesessionview") 
+	{
+		$url="https://uygulama-ebaders.eba.gov.tr/ders/FrontEndService//studytime/getteacherstudytime";
+		$buton=1;
+	} else if(value.substring(0,14)=="liveMiddleware") 
+	{
+		$url="https://uygulama-ebaders.eba.gov.tr/ders/FrontEndService//studytime/getstudentstudytime";
+		$buton=1;
+	} else $buton=0;
 });
 
-    if (document.getElementsByName('etudViewType') && $sayfa==1)
+    if (document.getElementsByName('etudViewType') && $buton==1)
+	{
+		 $("body").append($btn);
+	}
+	else $("#ekle").remove();
+
+	  
+   }
+});
+
+	
+  /*  if (document.getElementsByName('etudViewType'))
 	{
 		 $("body").append($r);
-	}
-*/
-    if (document.getElementsByName('etudViewType'))
-	{
-		 $("body").append($r);
-	}
+	}*/
 function goFrame() {
     $.ajax({
-  url : "https://uygulama-ebaders.eba.gov.tr/ders/FrontEndService//studytime/getstudentstudytime",
+  url : $url,
   method : "POST",
   headers : {
     "Content-Type" : "application/x-www-form-urlencoded",
     "Accept" : "json"
   },
-  data : "status=1&type=2&pagesize=25&pagenumber=0",
+  data : "status=2&type=1&pageNumber=1&pageSize=25",
   withCredentials : true,
   crossDomain : true,
   xhrFields : {
@@ -37,7 +59,9 @@ function goFrame() {
     var dersler = [];
     var dersText = "";
     var id = 1;
+	console.log(result);
     for (var i in result) {
+		
       if ((new Date).getTime() + 18000000 > result[i].startdate) {
         dersler.push(result[i]);
         dersText = dersText + (id.toString() + ") " + result[i].title + " (" + result[i].ownerName + ")\n");
@@ -50,6 +74,7 @@ function goFrame() {
     }
     var selectedDers = prompt("Seçim yapınız (sadece rakam girin):\n\n" + dersText);
     var ders = dersler[parseInt(selectedDers) - 1];
+	console.log(ders.id);
     $.ajax({
       url : "https://uygulama-ebaders.eba.gov.tr/ders/FrontEndService//livelesson/instudytime/start",
       method : "POST",
